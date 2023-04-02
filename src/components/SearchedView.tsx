61728ad05df2cc93ee147955af91c734f598c86e
yaohong6782 <yaohong6782@gmail.com>
2023-04-03 03:26:42 +0800

    url query for cover id

--- src/components/SearchedView.tsx
+++ src/components/SearchedView.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { searchManhwa } from "./lib/common";
+import { Card, CardBody } from "@chakra-ui/card";
 
 type SearchValue = {
   searchValue: string | null;
@@ -12,8 +13,15 @@ type Manhwa = {
   attributes?: Record<string, any> | undefined;
 };
 
+type ManhwaId = {
+  id: Record<string, string>;
+};
+const MANHWA_COVER_URL =
+  "https://api.mangadex.org/cover?limit=10&manga={manga_id}";
+
 const SearchedView = ({ searchValue }: SearchValue) => {
   const [manhwa, setManhwa] = useState<Manhwa[]>([]);
+  const [manhwaId, setManhwaId] = useState<string[]>([]);
 
   useEffect(() => {
     if (searchValue) {
@@ -21,6 +29,7 @@ const SearchedView = ({ searchValue }: SearchValue) => {
         .then((response) => {
           console.log(response.data);
           setManhwa(response.data);
+          setManhwaId(response.data.map((manga: ManhwaId) => manga.id));
         })
         .catch((error) => {
           console.log(error);
@@ -28,12 +37,52 @@ const SearchedView = ({ searchValue }: SearchValue) => {
     }
   }, [searchValue]);
 
+  console.log("cover ids ", manhwaId); // cover id
+
+  useEffect(() => {
+    if (manhwaId.length > 0) {
+      const mangaIdParam = manhwaId.map((id) => `manga%5B%5D=${id}`).join("&");
+      console.log('manga id param ', mangaIdParam)
+      const url = `https://api.mangadex.org/cover?limit=10&${mangaIdParam}`;
+      axios
+        .get(url)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [manhwaId]);
+
+  console.log("state ", manhwa); // entire response consisting of manga id
+
+  //   useEffect(() => {
+  //     if (manhwaId.length > 0) {
+  //       const coverPromises = manhwaId.map((id : any) => {
+  //         const url = MANHWA_COVER_URL.replace("{manga_id}", id);
+  //         return axios.get(url).then((response) => response.data);
+  //       });
+
+  //       Promise.all(coverPromises)
+  //         .then((coverResponses) => {
+  //           console.log(coverResponses); // An array of cover image data for each manga ID
+  //         })
+  //         .catch((error) => {
+  //           console.log(error);
+  //         });
+  //     }
+  //   }, [manhwaId]);
+
   return (
-    <div>
+    <div className="grid grid-cols-3 gap-3">
       {manhwa.map((item) => (
-        <div key={item.id}>
-          <h2>{item.attributes?.title?.en}</h2>
-        </div>
+        <Card maxW="lg" key={item.id}>
+          <CardBody>
+            <h2>{item.attributes?.title.en}</h2>
+            <h2>{item.id}</h2>
+          </CardBody>
+        </Card>
       ))}
     </div>
   );
--- src/components/lib/common.tsx
+++ src/components/lib/common.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const MANHWA_API = 'https://api.mangadex.org/manga?limit=10&title={}'
+export const GET_MANHWA_API = 'https://api.mangadex.org/manga?limit=10&title={}'
 
 export const searchManhwa = async (query : string) => {
-  const url = MANHWA_API.replace('{}', query);
+  const url = GET_MANHWA_API.replace('{}', query);
   const response = await axios.get(url);
   return response.data;
 }
