cb2f9dfac74ceb0c4f415fec0a2d41c6cea366ab
yaohong6782 <yaohong6782@gmail.com>
2023-04-05 03:52:45 +0800

    cover images progression

--- src/components/SearchedView.tsx
+++ src/components/SearchedView.tsx
@@ -21,6 +21,9 @@ const SearchedView = ({ searchValue }: SearchValue) => {
   const [manhwa, setManhwa] = useState<Manhwa[]>([]);
   const [manhwaId, setManhwaId] = useState<string[]>([]);
   const [manhwaCoverId, setManhwaCoverId] = useState<string[]>([]);
+  const [manhwaRelationship, setManhwaRelationship] = useState<
+    { mangaId: string; fileName: string }[]
+  >([]);
 
   useEffect(() => {
     if (searchValue) {
@@ -36,28 +39,40 @@ const SearchedView = ({ searchValue }: SearchValue) => {
     }
   }, [searchValue]);
 
-  console.log("manhwa ids ", manhwaId); // cover id
+    // console.log("manhwa ids ", manhwaId); // cover id
 
-  // grab cover id
+  interface CoverApiResponse {
+    data: {
+      id: string;
+      type: string;
+      attributes: {
+        fileName: string;
+      };
+      relationships: {
+        type: string;
+        id: string;
+      }[];
+    }[];
+  }
+
   useEffect(() => {
     if (manhwaId.length > 0) {
       const mangaIdParam = manhwaId.map((id) => `manga%5B%5D=${id}`).join("&");
-      console.log("manga id param ", mangaIdParam);
-      const url = `https://api.mangadex.org/cover?limit=10&${mangaIdParam}`;
+      const url = `https://api.mangadex.org/cover?limit=5&${mangaIdParam}`;
       axios
         .get(url)
-        .then((response) => {
-          console.log("cover id response ", response.data);
-          console.log(
-            "cover id response 2 ",
-            response.data.data.map((item: any) => item.attributes.fileName)
-          );
-
-          const coverFileName = response.data.data.map(
-            (item: any) => item.attributes.fileName
-          );
-          setManhwaCoverId(coverFileName);
-          //   setManhwaCoverId(response.data.attributes?.map((fileName : any) => fileName.fileName));
+        .then((response: { data: CoverApiResponse }) => {
+          console.log("second ", response.data);
+          const mangaIdsRelation = response.data.data.map((cover: any) => {
+            const mangaRelationship = cover.relationships.find(
+              (relationship: any) => relationship.type === "manga"
+            );
+            return {
+              mangaId: mangaRelationship.id,
+              fileName: cover.attributes.fileName,
+            };
+          });
+          setManhwaRelationship(mangaIdsRelation);
         })
         .catch((error) => {
           console.log(error);
@@ -65,27 +80,47 @@ const SearchedView = ({ searchValue }: SearchValue) => {
     }
   }, [manhwaId]);
 
-  console.log("cover id ", manhwaCoverId);
+  console.log("rs ", manhwaRelationship);
+
   const RETRIEVE_COVER_URL =
     "https://uploads.mangadex.org/covers/:manga-id/:cover-filename";
 
   useEffect(() => {});
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-5 gap-3 justify-items-center align-items-center">
-      {manhwa.map((item : any, index : number) => (
+      {/* {manhwa.map((item: any, index: number) => (
         <Card maxW="xs" key={item.id}>
           <CardBody>
-            <img
-              src={
-                `https://uploads.mangadex.org/covers/${item.id}/${manhwaCoverId[index]}`
-              }
-              alt=""
-            />
+            {manhwaRelationship[index] && (
+              <img
+                src={`https://uploads.mangadex.org/covers/${manhwaRelationship[index].mangaId}/${manhwaRelationship[index].fileName}`}
+                alt=""
+              />
+            )}
             <h2>{item.attributes?.title.en}</h2>
             <h2>{item.id}</h2>
           </CardBody>
         </Card>
-      ))}
+      ))} */}
+      {manhwa.map((item: any, index: number) => {
+        const cover = manhwaRelationship.find(
+          (c: any) => c.mangaId === item.id
+        );
+
+        return (
+          <Card maxW="xs" key={item.id}>
+            <CardBody>
+              {cover && (
+                <img
+                  src={`https://uploads.mangadex.org/covers/${cover.mangaId}/${cover.fileName}`}
+                  alt=""
+                />
+              )}
+              <h2>{item.attributes?.title.en}</h2>
+            </CardBody>
+          </Card>
+        );
+      })}
     </div>
   );
 };
