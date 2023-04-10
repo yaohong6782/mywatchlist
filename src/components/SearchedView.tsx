7b25b9cec1f57f6d6101e8d3b5ceecf0e272af30
yaohong6782 <yaohong6782@gmail.com>
2023-04-11 02:40:52 +0800

    new api url

--- src/components/SearchedView.tsx
+++ src/components/SearchedView.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { Card, CardBody } from "@chakra-ui/card";
+import { Image } from "@chakra-ui/react";
 
 type SearchValue = {
   searchValue: string | null;
@@ -12,75 +13,44 @@ type Manhwa = {
   attributes?: Record<string, any> | undefined;
 };
 
-type ManhwaId = {
-  id: Record<string, string>;
-};
-
 const SearchedView = ({ searchValue }: SearchValue) => {
-  const GET_MANHWA_API = `https://api.mangadex.org/manga?limit=10&title=${searchValue}`;
-  const RETRIEVE_COVER_URL =
-    "https://uploads.mangadex.org/covers/:manga-id/:cover-filename";
-
-  // const coverUrl = `https://uploads.mangadex.org/covers/${manhwa.id}/${manhwa.attributes?.coverArt?.[0]}.256.jpg`;
-  const [manhwaList, setManhwaList] = useState<any>([]);
-  const [manhwaArticles, setManhwaArticles] = useState<any>({});
-  const [manhwaCardsList, setManhwaCardsList] = useState<any>([]);
-  let manhwaIdListRef = useRef([]);
-  let manhwaIdList = [];
+  //   const baseUrl = "https://api.mangadex.org";
+  //   const GET_MANHWA_API = `${baseUrl}/manga?limit=10&title=${searchValue}&includes%5B%5D=cover_art`;
+  //   const REFERNCE_EXPANSION_API = `https://api.mangadex.org/manga/${mangaId}?includes[]=cover_art`
+  const baseUrl = "https://api.mangadex.org";
+  const GET_MANHWA_API = `${baseUrl}/manga`;
+  const [mangaData, setMangaData] = useState<any>();
+  const [mangaCardData, setMangaCardData] = useState<any>([]);
 
   useEffect(() => {
     if (searchValue) {
       axios
-        .get(GET_MANHWA_API)
+        .get(GET_MANHWA_API, {
+          params: {
+            limit: 10,
+            title: searchValue,
+            includes: ["cover_art"],
+          },
+        })
         .then((res) => {
-          //   console.log(res.data.data);
-          //   console.log(res.data.data[0].relationships);
-          setManhwaList(res.data.data);
-          manhwaIdListRef.current = res.data.data.map(
-            (mangaId: any) => mangaId.id
-          );
+          //   console.log(res.data);
+          setMangaData(res.data.data);
         })
         .catch((err) => {
           console.log(err);
         });
     }
-  }, [GET_MANHWA_API, searchValue]);
+  }, [searchValue, GET_MANHWA_API]);
+  console.log("manga data ", mangaData);
 
-  // grabbed list of cover ids
-  const manhwaCoverArtIds = manhwaList.map((manhwa: any) => {
-    const coverArtRelationship = manhwa.relationships.find(
-      (relationship: any) => relationship.type === "cover_art"
-    );
-    return coverArtRelationship.id;
+  let mangaID = mangaData?.map((item: any) => {
+    return item.id;
   });
 
-  console.log("cover ids ", manhwaCoverArtIds);
-  const idsUrl = manhwaCoverArtIds.join("&ids%5B%5D=");
-  const coverApiUrl = `https://api.mangadex.org/cover?ids%5B%5D=${idsUrl}`;
-  console.log("cover api url ", coverApiUrl);
-
-  // grabbing file name
-  useEffect(() => {
-    if (manhwaCoverArtIds.length > 0) {
-      axios
-        .get(coverApiUrl)
-        .then((res) => {
-          console.log("package ", res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  });
+  console.log(mangaID);
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-5 gap-3 justify-items-center align-items-center">
-      {manhwaList.map((manhwa: any, idx: number) => {
-        return (
-          <div key={idx}>
-            <p>{manhwa.attributes.title.en}</p>
-          </div>
-        );
-      })}
+      {}
     </div>
   );
 };
