e276086f6dccf7ca2fd59acff163dd552d7115a3
yaohong6782 <yaohong6782@gmail.com>
2023-04-11 03:35:27 +0800

    title and cover image displayed

--- src/components/SearchedView.tsx
+++ src/components/SearchedView.tsx
@@ -48,9 +48,23 @@ const SearchedView = ({ searchValue }: SearchValue) => {
   });
 
   console.log(mangaID);
+  //   console.log(mangaData)
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-5 gap-3 justify-items-center align-items-center">
-      {}
+      {mangaData?.map((item: any, idx: number) => {
+        const coverArtRelationship = item.relationships.find(
+          (rel: any) => rel.type === "cover_art"
+        );
+        const mangaId = item.id;
+        const coverArtId = coverArtRelationship?.id || "";
+        const coverArtFileName = coverArtRelationship?.attributes.fileName;
+        return (
+          <div key={idx}>
+            <p>{item.attributes?.title.en}</p>
+            <Image src={`https://uploads.mangadex.org/covers/${mangaId}/${coverArtFileName}`}/>
+          </div>
+        );
+      })}
     </div>
   );
 };
